e26ef62f08571242b823fcc6f986765d1c80c98c
ayafushimi <aya.fushimi@gmail.com>
2016-10-19 15:04:37 -0400

    defined method scrape_new_list, deleted comments

--- lib/recently_ps4_games/scraper.rb
+++ lib/recently_ps4_games/scraper.rb
@@ -18,16 +18,18 @@ class RecentlyPs4Games::Scraper
   end
 
   def self.scrape_new_list(url)
-    games = []
+    new_games_arr = []
     list_page = Nokogiri::HTML(self.get_dynamic_page_html(url))
-    game_grid = list_page.css("div.inlineTabs.section.gameGrid").first
+    new_game_grid = list_page.css("div.inlineTabs.section.gameGrid").first
+    new_games = new_game_grid.css("ul.clearfix li.layout-type-1 div.tile.clearfix div.game-tile-details h2 a.title")
 
-    binding.pry
-    
-    # new_game = game_grid.css("ul.clearfix li.layout-type-1").first
-    # title = new_game.css("div.game-tile-details h2 a.title").text
-    # detail_url = new_game.css("div.game-tile-details h2 a.title").attr("href").value
-
+    new_games.each do |new_game|
+      new_games_arr << {
+        title: new_game.text,
+        detail_url: new_game.attr("href")
+      }
+    end
+    new_games_arr
   end
 
 end
