a3e89cb399c636235839df3e16c711e47e333a20
ayafushimi <aya.fushimi@gmail.com>
2016-10-19 09:43:17 -0400

    change file name in lib

---
+++ lib/recently_ps4_games.rb
@@ -0,0 +1,5 @@
+require "recently_ps4_games/version"
+
+module RecentlyPs4Games
+  # Your code goes here...
+end
--- lib/recently_ps4_games_cli_gem.rb
+++
@@ -1,5 +0,0 @@
-require "recently_ps4_games_cli_gem/version"
-
-module RecentlyPs4GamesCliGem
-  # Your code goes here...
-end
