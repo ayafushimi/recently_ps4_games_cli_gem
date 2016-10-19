7b2e3b6b008158ab9898f012188960f2536bf6e1
ayafushimi <aya.fushimi@gmail.com>
2016-10-19 10:04:27 -0400

    add requires to recently_ps4_games.rb

--- lib/recently_ps4_games.rb
+++ lib/recently_ps4_games.rb
@@ -1,5 +1,11 @@
-require "recently_ps4_games/version"
+require 'pry'
+require 'open-uri'
+require 'nokogiri'
+
+require_relative "recently_ps4_games/cli.rb"
+require_relative "recently_ps4_games/game.rb"
+require_relative "recently_ps4_games/scraper.rb"
+require_relative "recently_ps4_games/version.rb"
 
 module RecentlyPs4Games
-  # Your code goes here...
 end
