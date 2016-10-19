260088661fb46683a843f56a9307e4eb04d38ac9
ayafushimi <aya.fushimi@gmail.com>
2016-10-19 19:16:50 -0400

    defined initialize method in Game class

--- lib/recently_ps4_games/game.rb
+++ lib/recently_ps4_games/game.rb
@@ -1,6 +1,13 @@
 class RecentlyPs4Games::Game
   attr_accessor :title, :detail_url, :discription, :release_date, :genre, :publisher, :developer
 
+  @@all = []
 
+  def initialize(game_hash)
+    game_hash.each do |k, v|
+      self.send("#{k}=", v)
+    end
+    @@all << self
+  end
 
 end
