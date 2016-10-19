a1dfcfeac05fc26e05aa23f81188fe8576de5f69
ayafushimi <aya.fushimi@gmail.com>
2016-10-19 11:30:53 -0400

    making scrape_list in Scraper class

--- lib/recently_ps4_games/scraper.rb
+++ lib/recently_ps4_games/scraper.rb
@@ -1,4 +1,24 @@
-class RecentlyPs4Games::Scraper
+require 'pry'
+require 'open-uri'
+require 'nokogiri'
 
+module RecentlyPs4Games
+end
+
+
+class RecentlyPs4Games::Scraper
+  def self.scrape_list(url)
+    games = []
+    list_page = Nokogiri::HTML(open(url))
+    game_grid = list_page.css("div.gameGrid")
+
+    binding.pry
+    # list_page.css("div.gameGrid")
+    # game = game_grid.css("div div div ul li div.game-tile div.tile.clearfix")
+
+
+  end
 
 end
+
+RecentlyPs4Games::Scraper.new.class.scrape_list("https://www.playstation.com/en-us/explore/games/ps4-games/")
