6320c0b625085d41bc0579e5893ca2aa64b01052
ayafushimi <aya.fushimi@gmail.com>
2016-10-19 13:33:37 -0400

    struggling to get dynamic webpage's html

--- lib/recently_ps4_games/scraper.rb
+++ lib/recently_ps4_games/scraper.rb
@@ -1,24 +1,27 @@
 require 'pry'
 require 'open-uri'
 require 'nokogiri'
+require 'watir-webdriver'
+require 'phantomjs'
 
 module RecentlyPs4Games
 end
 
 
 class RecentlyPs4Games::Scraper
-  def self.scrape_list(url)
+  def self.scrape_new_list(url)
     games = []
+    browser = Watir::Browser.new(:phantomjs)
     list_page = Nokogiri::HTML(open(url))
-    game_grid = list_page.css("div.gameGrid")
+    game_grid = list_page.css("div.inlineTabs.section.gameGrid").first
 
     binding.pry
     # list_page.css("div.gameGrid")
-    # game = game_grid.css("div div div ul li div.game-tile div.tile.clearfix")
+    # game = game_grid.css("ul.clearfix li.layout-type-1")
 
 
   end
 
 end
 
-RecentlyPs4Games::Scraper.new.class.scrape_list("https://www.playstation.com/en-us/explore/games/ps4-games/")
+RecentlyPs4Games::Scraper.new.class.scrape_new_list("https://www.playstation.com/en-us/explore/games/ps4-games/")
