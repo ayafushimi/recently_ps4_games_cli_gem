373aa258bb69b80ef58ac87884cc755861e8a241
ayafushimi <aya.fushimi@gmail.com>
2016-10-19 20:41:04 -0400

    added methods flow as comment in cli.rb

--- lib/recently_ps4_games.rb
+++ lib/recently_ps4_games.rb
@@ -11,8 +11,3 @@ require_relative "recently_ps4_games/version.rb"
 
 module RecentlyPs4Games
 end
-
-games_arr = RecentlyPs4Games::Scraper.scrape_new_list("https://www.playstation.com/en-us/explore/games/ps4-games/")
-
-RecentlyPs4Games::Game.create_by_games_arr(games_arr)
-RecentlyPs4Games::Game.get_detail_url_from_id(2)
--- lib/recently_ps4_games/cli.rb
+++ lib/recently_ps4_games/cli.rb
@@ -1,4 +1,15 @@
+module RecentlyPs4Games
+end
+
+
 class RecentlyPs4Games::Cli
 
 
 end
+
+# games_arr = RecentlyPs4Games::Scraper.scrape_new_list("https://www.playstation.com/en-us/explore/games/ps4-games/")
+#
+# RecentlyPs4Games::Game.create_by_games_arr(games_arr)
+# game = RecentlyPs4Games::Game.find_by_id(2)
+# details = RecentlyPs4Games::Scraper.scrape_details(game.detail_url)
+# game.add_attributes(details)
